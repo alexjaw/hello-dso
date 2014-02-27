1738e8a68d42a21f29d91a0a35dcae80ceaf334c
Alexander Jaworowski <alexander@jaworowski.se>
2014-02-27 15:23:45 +0100

    Added info about season for best observations

--- hello.py
+++ hello.py
@@ -1,1 +1,1 @@
-print "M35 is an open cluster and can be found in the Gemini constellation"
+print "M35 is an open cluster and can be found in the Gemini constellation. Especially during winter and fall"
